0f315c716623c9b26a052899b0f3b29898150ba3
cheble <chase.heble@gmail.com>
2015-03-24 19:34:47 -0500

    Added Views and the main Test class.

---
+++ src/test/view/InfoView.java
@@ -0,0 +1,9 @@
+package test.view;
+
+public class InfoView {
+
+	public InfoView() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/test/view/PlanetInfoView.java
@@ -0,0 +1,9 @@
+package test.view;
+
+public class PlanetInfoView extends InfoView {
+
+	public PlanetInfoView() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
