07efcb88463562da57de08f3b8a98fe82580e08d
cheble <chase.heble@gmail.com>
2015-03-24 19:55:03 -0500

    Added star model and update model data in PlanetInfoView.java

--- src/test/view/PlanetInfoView.java
+++ src/test/view/PlanetInfoView.java
@@ -2,7 +2,8 @@ package test.view;
 
 public class PlanetInfoView extends InfoView {
 
-	public PlanetInfoView() {
+	public PlanetInfoView(Object model) {
+		super(model);
 		// TODO Auto-generated constructor stub
 		// set dialog name
 		// set planet info
