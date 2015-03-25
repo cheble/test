d5cf3018aec87524834ad05ca16bc6f20eb82d26
Chase <chase.heble@gmail.com>
2015-03-24 19:54:38 -0500

    Update PlanetInfoView.java

--- src/test/view/PlanetInfoView.java
+++ src/test/view/PlanetInfoView.java
@@ -4,6 +4,9 @@ public class PlanetInfoView extends InfoView {
 
 	public PlanetInfoView() {
 		// TODO Auto-generated constructor stub
+		// set dialog name
+		// set planet info
+		// set planet image
 	}
 
 }
