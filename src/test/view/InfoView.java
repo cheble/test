ec4b2a20430bba8657f3b5200d9773e18fd7d5e9
Chase <chase.heble@gmail.com>
2015-03-30 15:02:51 -0500

    Update InfoView.java

--- src/test/view/InfoView.java
+++ src/test/view/InfoView.java
@@ -4,15 +4,25 @@ import javax.swing.JDialog;
 import javax.swing.JPanel;
 
 public class InfoView extends JPanel{
+	
+	private Object _model;
 
 	public InfoView(Object model) {
-		// TODO Auto-generated constructor stub
+		_model = model;
 	}
 	
 	public void showView() {
 		JDialog dlg = new JDialog();
 		dlg.add(this);
 		dlg.setVisible(true);
+	}
+	
+	public boolean isOpen() {
+		return this.isShowing();
+	}
+	
+	public Object getModel() {
+		return _model;
 	}
 
 }
