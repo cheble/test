58d54b68f270815d55a91dcf7af67dcfe4df9466
cheble <chase.heble@gmail.com>
2015-03-30 15:12:35 -0500

    Added attributes to the Planet model. Created some model objects in Test.main().

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,7 +1,9 @@
 package test;
 import javax.swing.JFrame;
 
+import test.models.Moon;
 import test.models.Planet;
+import test.models.Star;
 import test.view.InfoView;
 import test.view.PlanetInfoView;
 
@@ -14,8 +16,13 @@ public class Test {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Star sun = new Star();
+		Planet earth = new Planet();
+		Moon moon = new Moon();
+		Planet mars = new Planet();
+		Planet mercury = new Planet();
+		Planet venus = new Planet();
+		
 	}
 	
 	public void showPlanetDialog(Planet model) {
--- src/test/models/Planet.java
+++ src/test/models/Planet.java
@@ -1,6 +1,17 @@
 package test.models;
 
 public class Planet {
+	
+	private String _id;
+	
+	private String _name;
+	
+	// mass in solar mass
+	private double _mass;
+	
+	private double _radius;
+	
+	private Moon[] _moons;
 
 	public Planet() {
 		// TODO Auto-generated constructor stub
