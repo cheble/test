e0f94fb494a0aac45e37cd4285dd68feef7c40c0
cheble <chase.heble@gmail.com>
2015-03-30 15:08:50 -0500

    Added attributes to Star model.

--- src/test/models/Star.java
+++ src/test/models/Star.java
@@ -1,9 +1,25 @@
 package test.models;
 
 public class Star {
+	
+	private String _id;
+	
+	private String[] _names;
+	
+	// mass in solar mass
+	private double _mass;
+	
+	private double _luminosity;
+	
+	private double _radius;
+	
+	private double _age;
 
 	public Star() {
-		// TODO Auto-generated constructor stub
+	}
+
+	public Star(String id) {
+		_id = id;
 	}
 
 }
